4b66cb9996bd4c35b8dba820083dd118a8866eb5
kihyuks <kihyuk.sohn@gmail.com>
2023-05-17 10:03:14 -0700

    Update anon.py

--- anon.py
+++ anon.py
@@ -1,7 +1,7 @@
 # small script to anonymize the page for ICML submission
 output = True
 with open("index.html", "r") as f_in:
-    with open("icml.html", "w") as f_out:
+    with open("anon.html", "w") as f_out:
         for line in f_in:
             if "anon-on" in line:
                 output = False
